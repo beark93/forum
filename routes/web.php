b15498e0fa5a7cb5f78ea8b1967929cc003d63f4
beark93 <beark93@gmail.com>
2021-06-05 03:37:22 +0900

    게시글 리스트페이지 개발

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
+
+Route::get('/post', [PostController::class, 'getList']);
